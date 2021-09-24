60c98b8e27a040a7eefc4b14aac96f768a024473
amirrezaask <raskarpour@gmail.com>
2021-09-24 23:58:50 +0330

    use actix and sqlite3

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,55 @@
-#[macro_use]
-extern crate rocket;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::Row;
+use serde::{Deserialize, Serialize};
 
-mod models;
-use models::Todo;
-use rocket::serde::json::Json;
-use rocket::State;
-use std::sync::Arc;
-use std::sync::RwLock;
+static GET_ALL_TODOS: &str = r#"SELECT * FROM todos"#;
 
-struct TodosState(Arc<RwLock<Vec<Todo>>>);
+type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
+type Connection = r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager>;
 
-#[get("/todos")]
-async fn list(state: &State<TodosState>) -> Json<Vec<Todo>> {
-    let data = &*(state.0.read().unwrap());
-    Json(data.to_vec())
+#[derive(Serialize, Deserialize)]
+struct Todo {
+    title: String,
+    done: bool,
+}
+impl Todo {
+    fn from_row(row: &Row) -> Self {
+        Todo {
+            title: row.get_unwrap("title"),
+            done: row.get_unwrap("done"),
+        }
+    }
+}
+#[actix_web::get("/")]
+async fn index(db: web::Data<Pool>) -> impl Responder {
+    let conn = db.get().unwrap();
+    let mut stmt = conn.prepare(GET_ALL_TODOS).unwrap();
+    let mut rows = stmt.query([]).unwrap();
+    let mut todos: Vec<Todo> = Vec::new();
+    loop {
+        match rows.next() {
+            Ok(opt) => match opt {
+                Some(row) => todos.push(Todo::from_row(row)),
+                None => break,
+            },
+            Err(err) => {
+                println!("err {}", err);
+                break;
+            }
+        }
+    }
+
+    HttpResponse::Ok().json(todos)
 }
 
-#[post("/todos", data = "<todo>")]
-async fn add(state: &State<TodosState>, todo: Json<Todo>) -> Json<Todo> {
-    let mut d = state.0.write().unwrap();
-    d.push(todo.0.clone());
-    Json(todo.0)
-}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let manager = SqliteConnectionManager::file("todos.db");
+    let pool = Pool::new(manager).unwrap();
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .manage(TodosState(Arc::new(RwLock::new(vec![Todo::new(
-            String::from("First todo"),
-        )]))))
-        .mount("/", routes![list, add])
+    HttpServer::new(move || App::new().service(index).data(pool.clone()))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }

212781046e935cc7c843f4fba628d487f95cebb5
amirrezaask <raskarpour@gmail.com>
2021-09-25 20:51:06 +0330

    use sqlx instead of rusqlite

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,45 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use r2d2_sqlite::SqliteConnectionManager;
-use rusqlite::Row;
 use serde::{Deserialize, Serialize};
+use sqlx::SqlitePool;
 
 static GET_ALL_TODOS: &str = r#"SELECT * FROM todos"#;
-static ADD_TODO: &str = "INSERT INTO todos (title) VALUES (?1)";
-type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
+type Pool = sqlx::Pool<sqlx::sqlite::Sqlite>;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, sqlx::FromRow)]
 struct Todo {
     title: String,
     #[serde(default)]
     done: bool,
 }
-impl Todo {
-    fn from_row(row: &Row) -> Self {
-        Todo {
-            title: row.get_unwrap("title"),
-            done: row.get_unwrap("done"),
-        }
-    }
-}
+
 #[actix_web::post("/")]
 async fn new(db: web::Data<Pool>, todo: web::Json<Todo>) -> impl Responder {
-    let conn = db.get().unwrap();
-    let mut stmt = conn.prepare(ADD_TODO).unwrap();
-    db.get().unwrap().execute(ADD_TODO, &[&todo.title]).unwrap();
-
-    HttpResponse::Created()
+    let mut conn = db.acquire().await.unwrap();
+    let todo_id = sqlx::query("INSERT INTO todos (title) VALUES (?)")
+        .bind(&todo.title)
+        .execute(&mut conn)
+        .await
+        .unwrap()
+        .last_insert_rowid();
+    HttpResponse::Created().body(format!("{}", todo_id))
 }
+
 #[actix_web::get("/")]
 async fn index(db: web::Data<Pool>) -> impl Responder {
-    let conn = db.get().unwrap();
-    let mut stmt = conn.prepare(GET_ALL_TODOS).unwrap();
-    let mut rows = stmt.query([]).unwrap();
-    let mut todos: Vec<Todo> = Vec::new();
-    loop {
-        match rows.next() {
-            Ok(opt) => match opt {
-                Some(row) => todos.push(Todo::from_row(row)),
-                None => break,
-            },
-            Err(err) => {
-                println!("err {}", err);
-                break;
-            }
-        }
-    }
-
+    let mut conn = db.acquire().await.unwrap();
+    let todos: Vec<Todo> = sqlx::query_as(GET_ALL_TODOS)
+        .fetch_all(&mut conn)
+        .await
+        .unwrap();
     HttpResponse::Ok().json(todos)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let manager = SqliteConnectionManager::file("todos.db");
-    let pool = Pool::new(manager).unwrap();
+    let pool = SqlitePool::connect("todos.db").await.unwrap();
 
     HttpServer::new(move || App::new().service(index).service(new).data(pool.clone()))
-        .bind("127.0.0.1:8080")?
+        .bind("127.0.0.1:8000")?
         .run()
         .await
 }

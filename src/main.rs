693cbdf5c5a92edb7b4463188261abf49a8f064f
amirrezaask <raskarpour@gmail.com>
2021-09-25 12:00:17 +0330

    rocket impl

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,81 @@
 #[macro_use]
 extern crate rocket;
 
-mod models;
-use models::Todo;
-use rocket::serde::json::Json;
-use rocket::State;
-use std::sync::Arc;
-use std::sync::RwLock;
+use rocket::{http::Status, serde::json::Json};
 
-struct TodosState(Arc<RwLock<Vec<Todo>>>);
+use rocket_sync_db_pools::{database, rusqlite::Row};
+
+use serde::{Deserialize, Serialize};
+
+static GET_ALL_TODOS: &str = r#"SELECT * FROM todos"#;
+static ADD_TODO: &str = "INSERT INTO todos (title) VALUES (?1)";
+
+type Connection = rocket_sync_db_pools::rusqlite::Connection;
+
+#[database("todos")]
+struct TodosDb(Connection);
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Todo {
+    pub title: String,
+    #[serde(default)]
+    pub done: bool,
+}
+
+impl Todo {
+    pub fn new(title: String) -> Todo {
+        Todo { title, done: false }
+    }
+    pub fn from_row(row: &Row) -> Self {
+        Todo {
+            title: row.get_unwrap(1),
+            done: row.get_unwrap(2),
+        }
+    }
+}
 
 #[get("/todos")]
-async fn list(state: &State<TodosState>) -> Json<Vec<Todo>> {
-    let data = &*(state.0.read().unwrap());
-    Json(data.to_vec())
+async fn list(db: TodosDb) -> Json<Vec<Todo>> {
+    let todos = db
+        .run(|c| {
+            let mut stmt = c.prepare(GET_ALL_TODOS).unwrap();
+            let mut rows = stmt.query([]).unwrap();
+            let mut todos = Vec::new();
+            loop {
+                match rows.next() {
+                    Ok(opt) => match opt {
+                        Some(row) => todos.push(Todo::from_row(row)),
+                        None => break,
+                    },
+                    Err(err) => {
+                        println!("err {}", err);
+                        break;
+                    }
+                }
+            }
+            todos
+        })
+        .await;
+
+    Json(todos)
 }
 
 #[post("/todos", data = "<todo>")]
-async fn add(state: &State<TodosState>, todo: Json<Todo>) -> Json<Todo> {
-    let mut d = state.0.write().unwrap();
-    d.push(todo.0.clone());
-    Json(todo.0)
+async fn add(db: TodosDb, todo: Json<Todo>) -> Status {
+    db.run(move |c| {
+        c.prepare(ADD_TODO)
+            .unwrap()
+            .execute(&[&todo.0.title])
+            .unwrap()
+    })
+    .await;
+
+    Status::Created
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .manage(TodosState(Arc::new(RwLock::new(vec![Todo::new(
-            String::from("First todo"),
-        )]))))
+        .attach(TodosDb::fairing())
         .mount("/", routes![list, add])
 }
